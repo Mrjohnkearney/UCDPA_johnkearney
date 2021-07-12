f96593d1c9a7c9b4a32de1814800d4a736a9c8fa
Mrjohnkearney <85190193+Mrjohnkearney@users.noreply.github.com>
2021-07-12 22:57:16 +0100

    Fixing error with MErge

--- Import Datset.py
+++ Import Datset.py
@@ -3,9 +3,19 @@ import pandas as pd
 
 import os
 os.getcwd()
+import pandas as pd
+
+
 
 # Read CSV file
 Gender_Stats = pd.read_csv('Gender_StatsData.csv')
+# Creating a data frame
+df = pd.DataFrame(Gender_Stats)
+
+# Itering over the data frame rows
+# using df.iterrows()
+itr = next(df.iterrows())[1]
+itr
 
 # Print FIle
 print(Gender_Stats)
@@ -91,8 +101,73 @@ death_by_injury.sort_values(["2019"])
 # View Death by Injury DataFrame
 death_by_injury
 
-# NEw Dataset GDP_PEr_Capita
+
+# New Dataset GDP_Per_Capita
 GDP_Per_Capita =Gender_Stats_2019[Gender_Stats_2019["Indicator Name"] == 'GDP per capita (constant 2010 US$)']
 
 # Drop NaN values
 GDP = GDP_Per_Capita.dropna()
+
+# Create new file called GDP without NaN values
+GDP = GDP_Per_Capita.dropna()
+
+# Sort based on 2019 GDP per capita
+GDP.sort_values(["2019"])
+
+# Merge GDP and Death by Injury  using an innor join using country code as the key
+import pandas as pd
+
+pd.merge(GDP, death_by_injury, on='Country Code', how='inner')
+
+# Create a new dataframe called GDP_Death by Injury
+GDP_Death_By_Injury = pd.merge(GDP, death_by_injury, on='Country Code', how='inner')
+
+# Create a CSV file from the GDP_Death_by_Injury dataframe
+GDP_Death_By_Injury.to_csv("GDP_Death_By_Injury.csv", index=False, encoding='utf8')
+
+# Import Seaborn
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Loading Dataset:
+Death = pd.read_csv(r"C:\Users\jkearney\OneDrive - MyComplianceOffice\Documents\GitHub\UCDPA_johnkearney\GDP_Death_By_injury.csv")
+
+# previewing the Dataset
+Death.head(10)
+
+
+# Obtaining dataset  details:
+Death.describe()
+
+
+# Creating a Death csv file
+Death.to_csv("Death.csv", index=False, encoding='utf8')
+
+# Defining Color Ref https://medium.com/@neuralnets/data-visualization-with-python-and-seaborn-part-6-additional-linear-data-regression-plots-7a6fd469cea6
+
+tableau_20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
+         (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),
+         (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),
+         (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),
+         (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
+
+# Scaling avove RGB values to [0, 1] range, which is Matplotlib acceptable format:
+for i in range(len(tableau_20)):
+    r, g, b = tableau_20[i]
+    tableau_20[i] = (r / 255., g / 255., b / 255.)
+
+tableau_20[4]
+# Renameing the 2019_X and 2019_y Columns
+Death.rename(columns={ '2019_y':'% Death by Injury', '2019_x': 'GDP per Capita'})
+
+# Create new Dataset called Death New
+Death_New= Death.rename(columns={ '2019_y':'% Death by Injury', '2019_x': 'GDP per Capita'})
+
+
+
+# Plot Death new using Seaborn
+sns.lmplot(x='GDP per Capita', y= '% Death by Injury', data=Death_New, scatter_kws={'color':tableau_20[2], 'alpha':0.75})
+
+# Switch the X and Y axis
+sns.lmplot(x= '% Death by Injury', y = 'GDP per Capita', data=Death_New, scatter_kws={'color':tableau_20[2], 'alpha':0.75})
